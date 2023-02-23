44295aceb353e6693f92f205d6e9ad6ebfd34124
ellamajd <ellamajd@localhost>
2023-02-23 12:15:56 -0600

    You know whose flag this is.

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,29 @@
 package studio4;
 
+
+
 import java.awt.Color;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+	Color green = new Color(0, 100, 50);
+		StdDraw.setPenColor(green);
+	StdDraw.filledRectangle(0.5,0.5,1,0.5);
+	StdDraw.setPenColor(10,10,10);
+	StdDraw.filledRectangle(0.1, 0.5, 0.1, 0.5);
+	StdDraw.setPenColor(0,10,150);
+	StdDraw.filledCircle(0.5, 0.5, 0.1);
+	StdDraw.setPenColor(Color.YELLOW);
+	double[]x= {0.4,0.5,0.6};
+	double[]y= {0.6,0.7,0.6};
+	StdDraw.filledPolygon(x,y);
+	StdDraw.setPenColor(150,0,0);
+	StdDraw.arc(1, 0.5, 0.5, 60, 30);
+	StdDraw.setPenColor(150,0,0);
+	StdDraw.arc(1, 0.5, 0.25, 60, 30);
+	StdDraw.setPenColor(150,0,0);
+	StdDraw.arc(1, 0.5, 0.1, 60, 30);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,63 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+// for whatever shape
+		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		Color COLOR = new Color(red, green, blue);
+		boolean isFilled = in.nextBoolean();
+		double p1 = in.nextDouble();
+		double p2 = in.nextDouble();
+		double p3 = in.nextDouble();
+		double p4 = in.nextDouble();
+
+		if (shape.equals("ellipse")) {
+			StdDraw.setPenColor(COLOR);
+			if (isFilled == true) {
+				StdDraw.filledEllipse(p1, p2, p3, p4);
+			}
+			else {
+				StdDraw.ellipse(p1, p2, p3, p4);
+			}
+			
+			
+		}
+		else if (shape.equals("rectangle")) {
+			StdDraw.setPenColor(COLOR);
+			if (isFilled == true) {
+				StdDraw.filledRectangle(p1, p2, p3, p4);
+			}
+			else {
+				StdDraw.rectangle(p1, p2, p3, p4);
+			}
+			
+		}
+		else {
+			double p5= in.nextDouble();
+			double p6= in.nextDouble();
+			StdDraw.setPenColor(COLOR);
+			double []x = {p1, p3, p5};
+			double []y = {p2, p4, p6};
+			if (isFilled == true) {
+				StdDraw.filledPolygon(x,y);
+			}
+			else {
+				StdDraw.polygon(x,y);
+			}
+			
+		}
+		
+		
+		
+	
+		
+		
+		
+		
 		
 	}
 }
